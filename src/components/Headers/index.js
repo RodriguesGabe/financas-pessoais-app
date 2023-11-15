87eeb42ed8efcac18d20047d79ea1cf63bbce14c
Gitgabe <cybergabe01@gmail.com>
2023-11-15 17:24:06 -0300

    Mudancas de estilo no Header do app

--- src/components/Headers/index.js
+++ src/components/Headers/index.js
@@ -16,7 +16,7 @@ export default function Header(){
     return(
         <View style={styles.container}>
         <View style={styles.content}>
-        <Text style={styles.username}> Mina Financa </Text>
+        <Text style={styles.username}> Mina Financas </Text>
         
         <TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
             <Feather name="user" size={27} color='#FFF'> </Feather>
@@ -26,10 +26,27 @@ export default function Header(){
     )
 }
 
+//Mudancas de estilo do app
 const styles = StyleSheet.create({
     container:{
-        backgroundColor: '#8000FF',
+        backgroundColor: '#F08080',
         paddingTop: statusBarHeight,
+        flexDirection: 'row',
+        paddingStart: 16,
+        paddingEnd: 16,
+        paddingBottom: 44,
+    },
+    content: {
+        flex: 1,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    username:{
+        fontSize: 18,
+        color: '#FFF',
+        fontWeight: 'bold'
     }
+
 })
 
